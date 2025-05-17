c6c20834fff0236d35c9553ef30e20938c0b0165
harshvikram777 <harshvikrams507@gmail.com>
2025-05-18 00:24:57 +0530

    old files

--- Harsh.js
+++ Harsh.js
@@ -1,2 +1,9 @@
-console.log("harsh")
-console.log("meena")
+const accountID = "14445"
+let accountEmail = "ha@ha"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+ let accountState=""
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+let somenummber
---
+++ conversion.js
@@ -0,0 +1,18 @@
+let score="33"
+/*""no. can be convert into string*/
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33"=>33
+//"33abc"=> NaN
+// true=>1;false=>0
+let isloggedIn = "55"
+let harsh =Number(isloggedIn)
+let booleanisloggedIn = Boolean(isloggedIn)
+console.log(booleanisloggedIn)
+  console.log(typeof isloggedIn)
+  console.log(harsh)
+  console.log(typeof(harsh))
